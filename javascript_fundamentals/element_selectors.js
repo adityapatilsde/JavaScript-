01aeef6fa04a3de213c1ab0ebcfab817115e5e90
aditya <adityapatilsde@gmail.com>
2026-01-13 22:48:18 +0530

    java script basics

--- javascript_fundamentals/element_selectors.js
+++ javascript_fundamentals/element_selectors.js
@@ -15,7 +15,7 @@ const myHeading = document.getElementById("my-heading");
 myHeading.style.backgroundColor = "yellow";
 myHeading.style.textAlign = "center";
 
-console.log(myHeading);
+console.log(myHeadings);
 
 //==========================================================================================
 //CODE 2 
