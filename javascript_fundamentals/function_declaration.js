ac49279cd5ef963026e9242fb14b755d3d7d34e7
aditya <adityapatilsde@gmail.com>
2026-02-14 22:05:42 +0530

    inheritance in js

--- javascript_fundamentals/function_declaration.js
+++ javascript_fundamentals/function_declaration.js
@@ -20,3 +20,30 @@ setTimeout(hello ,6000);
 
 /* that is 6000 is the milisecond timer after 6000 miliseconds
 the output will be shown in the console*/
+//CODE 10 
+
+// Parent class
+class Payment {
+    constructor(amount) {
+        this.amount = amount;
+    }
+
+    processPayment() {
+        console.log("Processing payment of ₹" + this.amount);
+    }
+}
+
+// Child class
+class CreditCardPayment extends Payment {
+    constructor(amount, cardNumber) {
+        super(amount);   // call parent constructor
+        this.cardNumber = cardNumber;
+    }
+
+    showCard() {
+        console.log("Paid using card ending with " + this.cardNumber.slice(-4));
+    }
+}
+
+// Create object
+let p1 = new CreditC
