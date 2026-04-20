e2a5f03bf8b7e2880161178ac9b3d0c6cfb4fe70
aditya <adityapatilsde@gmail.com>
2026-04-21 02:20:29 +0530

    started with testimonials page in my coffee web site

--- Projects/coffee_website/script.js
+++ Projects/coffee_website/script.js
@@ -10,3 +10,19 @@ menuOpenButton.addEventListener("click", () => {
 
 menuCloseButton.addEventListener("click" , () => menuOpenButton.click
 ());
+
+// Initialize Swiper
+const swiper = new Swiper('.swiper-wrapper', {
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
